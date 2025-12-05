e5f6a3f3afb9f3c9863f10d786df160d4b3e1672
Dexin Huang <dh3172@columbia.edu>
2025-12-04 21:23:17 -0500

    Add proxy to fix CORS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://alpha-boost-service-tbmfdv7fhq-uc.a.run.app/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
